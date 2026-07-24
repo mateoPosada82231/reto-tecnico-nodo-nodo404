1c9e2eff227fb6ea624b8eacc5db30e566331d7e
mateoPosada82231 <mateo_posada82231@elpoli.edu.co>
2026-07-24 17:43:50 -0500

    refactor: implement auth providers

---
+++ src/main/java/com/nodo/retotecnico/dto/UserResponseDTO.java
@@ -0,0 +1,21 @@
+package com.nodo.retotecnico.dto;
+
+import com.nodo.retotecnico.models.AuthProvider;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class UserResponseDTO {
+    private String email;
+    private String fullName;
+    private AuthProvider provider;
+    private String providerId;
+    private String country;
+    private String identification;
+    private String mobileNumber;
+    private LocalDate dateOfBirth;
+    private boolean profileComplete;
+}
